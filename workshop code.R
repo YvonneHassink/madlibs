df5b8606082eaf174576cfd3b7e938b4b219177c
YvonneHassink <45395743+YvonneHassink@users.noreply.github.com>
2024-08-12 11:29:03 -0700

    created manifest

--- workshop code.R
+++ workshop code.R
@@ -2,3 +2,4 @@ library(usethis)
 create_project("Developer/posit/madlibs")
 use_git()
 use_github()
+rsconnect::writeManifest() #eerst file rename naar app.r anders krijg ik een foutmelding. dan weet hij niet welke file?
