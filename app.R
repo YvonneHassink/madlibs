6d815cbfe516f96dc4c6ee530e042dc97283aa2c
YvonneHassink <45395743+YvonneHassink@users.noreply.github.com>
2024-08-12 14:06:24 -0700

    with some logging

--- app.R
+++ app.R
@@ -1,6 +1,11 @@
 library(shiny)
 
 generate_story <- function(noun, verb, adjective, adverb) {
+  cat("\This is a message\ with the noun:{noun}", file = stderr())
+  cat("\This is a message\ with the verb:{verb}", file = stderr())
+  cat("\This is a message\ with the adjective:{adjective}", file = stderr())
+  cat("\This is a message\ with the adverb:{adverb}", file = stderr())
+
   glue::glue("
     Once upon a time, there was a {adjective} {noun} who loved to
     {verb} {adverb}. It was the funniest thing ever!
--- workshop code.R
+++ workshop code.R
@@ -3,3 +3,6 @@ create_project("Developer/posit/madlibs")
 use_git()
 use_github()
 rsconnect::writeManifest() #eerst file rename naar app.r anders krijg ik een foutmelding. dan weet hij niet welke file?
+
+pak::pak("hadley/useself")
+library("useself")
