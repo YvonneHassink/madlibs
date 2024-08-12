37f9503390c2eedf58f1a203f1b64392d0c647d8
YvonneHassink <45395743+YvonneHassink@users.noreply.github.com>
2024-08-12 11:39:53 -0700

    tekstuele aanpassing om te checken hoe dit deployed

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ generate_story <- function(noun, verb, adjective, adverb) {
   glue::glue("
     Once upon a time, there was a {adjective} {noun} who loved to
     {verb} {adverb}. It was the funniest thing ever!
+    Ik voeg hier iets tekst toe
   ")
 }
 
